84520ed6a91af5d6fa22cf4b7ce2f2aea63fab57
mounika26 <mounika.kalakoti@gmail.com>
2024-02-20 20:23:43 +0530

    Swapping Integers

--- mouni/SwapIntegers.ts
+++ mouni/SwapIntegers.ts
@@ -1,8 +1,8 @@
-let a: number[] = 20;
-let b = 30;
+let aa: number = 20;
+let bb = 30;
 
-const temp = a;
-a = b;
-b = temp;
-console.log(a);
-console.log(b);
+const temp = aa;
+aa = bb;
+bb = temp;
+console.log(aa);
+console.log(bb);
