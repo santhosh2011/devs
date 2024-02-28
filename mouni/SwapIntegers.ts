0dac76bcb4a7d21c9a5de03180fd0032a6b07154
mounika26 <mounika.kalakoti@gmail.com>
2024-02-28 20:04:23 +0530

    Sorting using swap function

--- mouni/SwapIntegers.ts
+++ mouni/SwapIntegers.ts
@@ -1,8 +1,18 @@
-let aa: number = 20;
-let bb = 30;
+function swapArray(arr: number[], index1: number, index2: number): number[] {
+  const temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+  return arr;
+}
+console.log(swapArray([1, 2, 3, 4, 7, 9, 6], 3, 5));
 
-const temp = aa;
-aa = bb;
-bb = temp;
-console.log(aa);
-console.log(bb);
+// selection_sort: using above method for swapping
+function sortArray(a: number[]): number[] {
+  for (let i = 0; i < a.length; i++) {
+    for (let j = i; j < a.length; j++) {
+      swapArray(a, i, j);
+    }
+  }
+  return a;
+}
+console.log(sortArray([1, 2, 3, 4]));
