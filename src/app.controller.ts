c85ffb062164efcae73f5af94bbf0b86a5819019
mounika26 <mounika.kalakoti@gmail.com>
2024-03-05 21:00:45 +0530

    userDetails using Get()

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { getUserByName } from './user.service';
 
 @Controller()
 export class AppController {
@@ -9,5 +10,36 @@ export class AppController {
   getHello(): string {
     // this is a sample change
     return this.appService.getHello();
+  }
+
+  @Get('name')
+  getName(): string {
+    return 'Name';
+  }
+
+  @Get('userDetails')
+  getUserDetails(@Query() query): any {
+    const users = [
+      { firstname: 'santhosh', lastname: 'thota', email: 'santhosh@gmail.com' },
+      {
+        firstname: 'santhosh',
+        lastname: 'kumar',
+        email: 'santhoshkumar@gmail.com',
+      },
+      {
+        firstname: 'vinay',
+        lastname: 'bandila',
+        email: 'vinay@gmail.com',
+      },
+      { firstname: 'mouni', lastname: 'kalakoti', email: 'mouni@gmail.com' },
+    ];
+
+    const queryfirstName = query['firstname'];
+    const querylastName = query['lastname'];
+
+    if (queryfirstName == null || querylastName == null) {
+      return users;
+    }
+    return getUserByName(users, queryfirstName, querylastName);
   }
 }
